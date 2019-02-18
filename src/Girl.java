433cd4e5ad782cc57d80ee4eeb98137e447ec6f7
Fotus <jayweason@qq.com>
2019-02-18 11:14:35 +0800

    second commit

--- src/Girl.java
+++ src/Girl.java
@@ -1,3 +1,12 @@
 public class Girl {
+    private String name;
+    private int age;
 
+    public Girl() {
+    }
+
+    public Girl(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
