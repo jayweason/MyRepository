6b6fbd80adb311ce355ee2ca0b8b01be5d3bb664
Fotus <jayweason@qq.com>
2019-02-18 10:10:36 +0800

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,28 @@ public class Main {
         // git init
         // 注:当前文件夹中,有文件要进行版本控制
 
+        // git信息配置(首次登陆)
+        // git config --global user.name "姓名"
+        // git config --global user.email "邮箱"
+
+        // 查看git信息配置
+        // git config --global --list
+
+        // 注:
+        // --system: 系统
+        // --global: 当前用户
+        // --local: 当前仓库
+
         // 查看当前版本库的状态
         // git status
 
         // 指定某个文件进行版本控制
         // git add
+
+        // 红色: 未进行版本控制的文件
+        // 绿色: 新增文件
+
+        // 提交修改
+        // git commit -m "注释"
     }
 }
